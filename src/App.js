31d19fe04e88f4273b6a12b41c7d2b1d3079cadd
Jessica Cousins <113946660+jessicacousins@users.noreply.github.com>
2023-09-20 23:15:12 -0400

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { marked } from "marked";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [markdown, setMarkdown] = useState(`# Heading 1
+## Heading 2
+[Link to Google](https://www.google.com)
+\`inline code\`
+\`\`\`
+// Code Block
+const greeting = "Hello World!!!";
+console.log(greeting);
+\`\`\`
+### Heading 3
+- List Item 1
+- List Item 2
+- List Item 3
+- List Item 4
+
+#### Heading 4
+> Blockquote
+
+##### Heading 5
+
+![Image](https://via.placeholder.com/150)
+
+**Bold Text - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, doloribus.**`);
+
+  const handleInputChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const html = marked(markdown);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="markdown-container">
+      <h1 className="text-center mt-4" style={{ color: "#fff" }}>
+        Markdown Previewer
+      </h1>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="editor-container">
+            <h2 className="text-center">Editor</h2>
+            <textarea
+              id="editor"
+              onChange={handleInputChange}
+              value={markdown}
+            ></textarea>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="preview-container">
+            <h2 className="text-center">Preview</h2>
+            <div id="preview" dangerouslySetInnerHTML={{ __html: html }}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
